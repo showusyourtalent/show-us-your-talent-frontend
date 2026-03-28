b57b8aae516b9be2eed2117692ebdf6047030bc9
dglink25 <dglink25@gmail.com>
2026-03-28 17:54:37 +0100

    Correction url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+
 const API_URL = import.meta.env.VITE_API_URL || 'https://show-us-your-talent-backend-main-qouoel.free.laravel.cloud/api';
+
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
